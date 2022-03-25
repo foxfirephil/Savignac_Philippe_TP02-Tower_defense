aae9ab2db6d1fc99a3df27d66e0cef98c3991495
Mr. Pickle <101284114+foxfirephil@users.noreply.github.com>
2022-03-25 10:34:30 -0400

    making spawn work
    test pour faire apparaitre les préfabs

--- tp2-tower_defence_ps/Assets/script/spawn.cs
+++ tp2-tower_defence_ps/Assets/script/spawn.cs
@@ -6,13 +6,17 @@ public class spawn : MonoBehaviour
 {
 
     public GameObject zombprefab;
+    public GameObject warokprefab;
+    public GameObject nshdprefab;
     public int level;
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < level; i++)
         {
-            Instantiate(zombprefab, transform.position + transform.forward,Quaternion.identity);
+            Instantiate(zombprefab, transform.position,Quaternion.identity);
+            Instantiate(warokprefab, transform.position,Quaternion.identity);
+            Instantiate(nshdprefab, transform.position,Quaternion.identity);
         }
     }
 
